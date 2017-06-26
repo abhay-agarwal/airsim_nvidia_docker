d615861ea713eb15775a4135d23442b84abb4601
abhay-agarwal <aabhay@gmail.com>
2017-06-25 21:59:43 -0700

    run script for airsim unreal builder

--- run.sh
+++ run.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-xhost +local:root; \
-docker run -it \
-     --env="DISPLAY" \
-     --env="QT_X11_NO_MITSHM=1" \
-     --device="/dev/nvidia0:/dev/nvidia0" \
-     --device="/dev/nvidiactl:/dev/nvidiactl" \
-     --device="/dev/nvidia-modeset:/dev/nvidia-modeset" \
-     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-     --volume="$PWD/Documents:/home/unreal/Documents" \
-     --cap-add sys_nice \
-     --net=host \
-     airsim_nvidia /home/unreal/Documents/launch.sh
+docker build -t airsim-unreal-builder . && docker run -v $(pwd):/ext airsim-unreal-builder cp -r /home/unreal/out/LinuxNoEditor /ext/cooked/

14f1c43f1bab4344441557955930b3dd1e47ab76
Abhay Agarwal <abhay.agarwal@berkeley.edu>
2017-06-26 10:24:24 -0700

    better docker file

--- cooked/docker.sh
+++ cooked/docker.sh
@@ -1,7 +1,1 @@
-nvidia-docker run \
--e PORT \
--p 590$PORT:590$PORT \
--p 4145$PORT:41451 \
--v /tmp/.X11-unix:/tmp/.X11-unix \
---volume="/usr/lib/x86_64-linux-gnu/libXv.so.1:/usr/lib/x86_64-linux-gnu/libXv.so.1"
--it airsim-linux
+nvidia-docker run -e PORT -p 590$PORT:590$PORT -p 4145$PORT:41451 -v /tmp/.X11-unix:/tmp/.X11-unix --volume="/usr/lib/x86_64-linux-gnu/libXv.so.1:/usr/lib/x86_64-linux-gnu/libXv.so.1" -it airsim-linux

5cb3c3c2c0139671573962bdf090c37a9d7db4b7
abhay-agarwal <aabhay@gmail.com>
2017-06-26 16:10:57 -0700

    launch fix x bug

--- cooked/launch.sh
+++ cooked/launch.sh
@@ -5,7 +5,7 @@ tmux new-window -t airsim:1 -n sitl -c /home/unreal/Firmware
 tmux new-window -t airsim:2 -n sim -c /home/unreal/Blocks/Binaries/Linux/
 
 tmux send-keys -t airsim:1 "sudo ./build_posix_sitl_default/src/firmware/posix/px4 ~/Documents/px4_sitl_airsim" C-m
-tmux send-keys -t airsim:0 "Xvnc :$PORT -rfbport 590$PORT -geometry 284x160 -SecurityTypes None" C-m
+tmux send-keys -t airsim:0 "sudo rm -f /tmp/.X$PORT-lock; Xvnc :$PORT -rfbport 590$PORT -geometry 284x160 -SecurityTypes None" C-m
 sleep 1
 tmux send-keys -t airsim:2 "DISPLAY=:$PORT vglrun ./Blocks-Linux-Shipping -FULLSCREEN -ResX 284 -ResY 160 -VSync" C-m
 

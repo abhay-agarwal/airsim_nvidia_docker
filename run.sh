824ce22e2b62725f426b793580d70b4c54a7b838
Abhay Kumar Agarwal <abhay.agarwal@berkeley.edu>
2017-06-26 17:32:35 +0000

    better run

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
-docker build -t airsim-unreal-builder . && docker run -v $(pwd):/ext airsim-unreal-builder cp -r /home/unreal/out/LinuxNoEditor /ext/cooked/
+set -e
+set -x
+docker build -t airsim-unreal-builder . 
+sudo rm -rf ./cooked/LinuxNoEditor
+docker run -v $(pwd):/ext airsim-unreal-builder cp -r /home/unreal/out/LinuxNoEditor /ext/cooked/

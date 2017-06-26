d7c0261a8798f6feafaf3587687ea41773d518f8
abhay-agarwal <aabhay@gmail.com>
2017-06-26 14:01:48 -0700

    remove stale docker in bash script

--- cooked/docker.sh
+++ cooked/docker.sh
@@ -1,1 +1,3 @@
-nvidia-docker run -e PORT -p 590$PORT:590$PORT -p 4145$PORT:41451 -v /tmp/.X11-unix:/tmp/.X11-unix --volume="/usr/lib/x86_64-linux-gnu/libXv.so.1:/usr/lib/x86_64-linux-gnu/libXv.so.1" -it airsim-linux
+docker kill airsim-0$PORT
+docker rm airsim-0$PORT
+nvidia-docker run --name airsim-0$PORT -e PORT -p 590$PORT:590$PORT -p 4145$PORT:41451 -v /tmp/.X11-unix:/tmp/.X11-unix --volume="/usr/lib/x86_64-linux-gnu/libXv.so.1:/usr/lib/x86_64-linux-gnu/libXv.so.1" -it airsim-linux

48c3a625678bfdad73c80d9b1b111cd05e56be71
abhay-agarwal <aabhay@gmail.com>
2017-06-27 10:09:02 -0700

    separate run and watch script

--- cooked/run8.sh
+++ cooked/run8.sh
@@ -6,20 +6,8 @@ if [ ! -d "LinuxNoEditor" ] ; then
 fi
 
 docker build -t airsim-linux .
-
-# stop any existing 
-tmux kill-session -t nvidia-docker || true
-docker kill $(docker ps -q) || true
-
-tmux new-session -s nvidia-docker -n htop -d
-tmux send-keys -t nvidia-docker:0 "htop" C-m
-
-# start from port 2
 for PORT in 2 3 4 5 6 7 8 9; do
-	 docker rm airsim-0$PORT || :
-    tmux new-window -t nvidia-docker:$PORT -n instance$PORT
-    tmux send-keys -t nvidia-docker:$PORT "PORT=$PORT sh docker.sh" C-m
-done
+	 docker kill airsim-0$PORT || rm airsim-0$PORT || :
+    PORT=$PORT sh docker.sh
 
-tmux select-window -t nvidia-docker:0
-tmux attach-session -t nvidia-docker
+docker ps

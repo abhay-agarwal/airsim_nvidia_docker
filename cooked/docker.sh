f0ab8b8abbef77cdfa011a8de65f135429ceea24
abhay-agarwal <aabhay@gmail.com>
2017-06-27 00:02:01 -0700

    always restart

--- cooked/docker.sh
+++ cooked/docker.sh
@@ -1,3 +1,3 @@
 docker kill airsim-0$PORT
 docker rm airsim-0$PORT
-nvidia-docker run --name airsim-0$PORT --restart=unless-stopped -e PORT -p 590$PORT:590$PORT -p 4145$PORT:41451 -v /tmp/.X11-unix:/tmp/.X11-unix --volume="/usr/lib/x86_64-linux-gnu/libXv.so.1:/usr/lib/x86_64-linux-gnu/libXv.so.1" -it airsim-linux
+nvidia-docker run --name airsim-0$PORT --restart=on-failure -e PORT -p 590$PORT:590$PORT -p 4145$PORT:41451 -v /tmp/.X11-unix:/tmp/.X11-unix --volume="/usr/lib/x86_64-linux-gnu/libXv.so.1:/usr/lib/x86_64-linux-gnu/libXv.so.1" -it airsim-linux
